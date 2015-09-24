c7e8e3ef79ae421626b8e6572be851fa40dbca4c
Kurtis Graben <kurtis.graben@me.com>
2015-09-23 20:52:48 -0500

    MAKES TESTFORNUMBER UNIT TEST WORK
    Makes unit test testForNumberIsTrue returns true if a number is entered.
    
    Makes unit test testForNumberIsFalse returns false if a string is
    entered.

--- ArgumentParser/src/test/java/edu/jsu/mcis/ArgumentParserTest.java
+++ ArgumentParser/src/test/java/edu/jsu/mcis/ArgumentParserTest.java
@@ -7,72 +7,65 @@ import java.util.*;
 
 public class ArgumentParserTest {
 
-
 	private ArgumentParser AP;
-	
-	
+
 	@Before
 	public void startUp(){
-		
-	
 		AP=new ArgumentParser();
 	}
-	
-	
+
 	@Test
-	
+
 	public void testOnly3numbersEntered(){
-		
+
 		AP.addPositionalArguments("length");
 		AP.addPositionalArguments("width");
 		AP.addPositionalArguments("height");
 		//AP.addPositionalArguments("other");
 		String[] data= {"7","5","2"};
-		
+
 		//AP.parse(data);
 		//(length,7);
 		//(width,5);
 		//(height,2);
-		
+
 		assertEquals(AP.getMessage(data), "just enough variables");
-		
-	
 	}
-	
-	
-	
+
 	@Test
-	
+
 	public void testLessThan3NumbersEntered(){
-	
 		AP.addPositionalArguments("length");
 		AP.addPositionalArguments("width");
 		AP.addPositionalArguments("height");
 		String[] data= {"7","5"};
 		//AP.parse(data);
 		assertEquals(AP.getMessage(data), "usage: java VolumeCalculator length width heigh. VolumeCalculator.java: error: the following arguments are required: height");
-	
-	
-	
 	}
-	
-	
+
 	@Test
-	
+
 	public void testMoreThan3NumbersEntered(){
-	
 		AP.addPositionalArguments("length");
 		AP.addPositionalArguments("width");
 		AP.addPositionalArguments("height");
 		String[] data= {"7","5", "3", "7"};
 		//AP.parse(data);
 		assertEquals(AP.getMessage(data), "usage: java VolumeCalculator length width height. VolumeCalculator.java: error: unrecognized arguments:" + data[3]);
-	
-	
-	
 	}
-	
 
+	@Test
+
+	public void testNumbersBeingEnteredReturnsTrue() {
+		String s = "1";
+		assertTrue(AP.checkIfNumber(s));
+	}
+
+	@Test
+
+	public void testNumberBeingEnteredReturnsFalse() {
+		String s = "test";
+		assertFalse(AP.checkIfNumber(s));
+	}
 
 }
-	
