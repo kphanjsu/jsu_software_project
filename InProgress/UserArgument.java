9c455d5b17d2e1cf2a8f210c472a2e64e9b8cd01
Christopher Burdette <cburdette@jsu.edu>
2015-09-27 12:31:02 -0500

    exception classes & other

---
+++ InProgress/UserArgument.java
@@ -0,0 +1,27 @@
+package edu.jsu.mcis;
+
+public class UserArgument{
+	
+	String argument="";
+	
+	
+	public void setArgument(String s){
+		argument=s;
+	
+	
+	
+	}
+	
+	
+	
+	public String getArgument(){
+		return argument;
+	
+	}
+
+
+
+
+
+
+}
